c9da4f8a4fffc6c33e0a031b305cffce0c74aba5
Lucas Almeida <lucas.a.souza@outlook.com>
2017-05-27 21:36:01 -0300

    crud finished

---
+++ config/config.php
@@ -0,0 +1,7 @@
+<?php
+    define('ROOT_URL', 'http://localhost/traversyMedia/phpFrontToBack/website8/');
+    define('DB_HOST', 'localhost');
+    define('DB_USER', 'root');
+    define('DB_PASS', 'root');
+    define('DB_NAME', 'phpblog');
+?>
---
+++ inc/header.php
@@ -0,0 +1,8 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>PHP Blog</title>
+        <link rel="stylesheet" href="https://bootswatch.com/cerulean/bootstrap.min.css">
+    </head>
+    <body>
+    <?php include('navbar.php'); ?>
